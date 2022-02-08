49b4bfb98dda13b2fc5ff0a5b34d6250b766672e
Adeel Butt <adeel.butt.hh@gmail.com>
2022-02-08 14:06:55 +0100

    added first story

--- style/components/card.stories.js
+++ style/components/card.stories.js
@@ -1,7 +1,7 @@
-import DefaultHtml from "./Card.default.html";
+import defaulthtml from "./card.default.html";
 
 export default {
   title: "Components/Card",
 };
 
-export const Default = () => DefaultHtml;
+export const Default = () => defaulthtml;
